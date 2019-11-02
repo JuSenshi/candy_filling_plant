28444f1ed120d4f9fc7e46afa19b88ffc138b7a6
KriegersBlog <53866271+KriegersBlog@users.noreply.github.com>
2019-11-02 21:42:04 +0100

    Commiting..

--- Lieferungssystem/Coding/Coding.ino
+++ Lieferungssystem/Coding/Coding.ino
@@ -2,9 +2,9 @@
 
 //Pins deklarieren
 const int forward_left = 3;
-const int forward_right = 4;   
+const int forward_right = 4;
 const int backward_left = 2;
-const int backward_right = 5;  
+const int backward_right = 5;
 const int sensor_left = A0;
 const int sensor_right = A1;
 
@@ -19,39 +19,36 @@ const int stop = 0b1111;
 const int sensor_limit = 100;
 
 void setup() {
-  pinMode(forward_left,OUTPUT);
-  pinMode(forward_right,OUTPUT);
-  pinMode(backward_left,OUTPUT);
-  pinMode(backward_right,OUTPUT);
+  pinMode(forward_left, OUTPUT);
+  pinMode(forward_right, OUTPUT);
+  pinMode(backward_left, OUTPUT);
+  pinMode(backward_right, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  
+
   //Beide über Grenzwert -> Vorwärts
-  while(analogRead(sensor_left) <= sensor_limit && analogRead(sensor_right) <= sensor_limit){
-    for(int i = 0; i<1; i++) 
-      fahren(0b0000);
+  while (analogRead(sensor_left) <= sensor_limit && analogRead(sensor_right) <= sensor_limit) {
+    fahren(0b0000);
     fahren(forward);
-    
+
   }
   //Links unter Grenzwert, rechts über Grenzwert -> Links
-  while(analogRead(sensor_left) <= sensor_limit && analogRead(sensor_right) >= sensor_limit){
-    for(int i = 0; i<1; i++) 
-      fahren(0b0000);
-      fahren(right);
+  while (analogRead(sensor_left) <= sensor_limit && analogRead(sensor_right) >= sensor_limit) {
+    fahren(0b0000);
+    fahren(right);
   }
   //Links über Grenzwert, rechts unter Grenzwert -> Rechts
-  while(analogRead(sensor_left) >= sensor_limit && analogRead(sensor_right) <= sensor_limit){
-    for(int i = 0; i<1; i++) 
-      fahren(0b0000);
+  while (analogRead(sensor_left) >= sensor_limit && analogRead(sensor_right) <= sensor_limit) {
+    fahren(0b0000);
     fahren(left);
   }
 }
 
-void fahren(uint8_t value){
- digitalWrite(backward_right, value&0b1000); 
- digitalWrite(backward_left, value&0b0100);
- digitalWrite(forward_right, value&0b0010);
- digitalWrite(forward_left, value&0b0001);   
-  }
+void fahren(uint8_t value) {
+  digitalWrite(backward_right, value & 0b1000);
+  digitalWrite(backward_left, value & 0b0100);
+  digitalWrite(forward_right, value & 0b0010);
+  digitalWrite(forward_left, value & 0b0001);
+}
